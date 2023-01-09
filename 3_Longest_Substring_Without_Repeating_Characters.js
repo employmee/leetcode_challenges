bdd84b30b5056d4c7fba23c6a692001333ed64e6
wanxin pua <wanxinpua@yahoo.com.sg>
2023-01-09 23:03:27 +0800

    added comments to problem 3

--- 3_Longest_Substring_Without_Repeating_Characters.js
+++ 3_Longest_Substring_Without_Repeating_Characters.js
@@ -8,19 +8,19 @@
   let longestString = ''
   let currentString = ''
   for (let i=0; i<s.length; i++) {
-      if (chars[s[i]]===undefined || chars[s[i]] < chars[currentString[0]]) {
+      if (chars[s[i]]===undefined || chars[s[i]] < chars[currentString[0]]) { // If not repeating character, or repeated char is not in current substring (not applicable)
           chars[s[i]] = i; // Insert/replace char in the chars object with index as the value
           currentString = currentString + s[i]; // add char to substring
-          if (i===s.length-1 && currentString.length > longestString.length) {
+          if (i===s.length-1 && currentString.length > longestString.length) { // If reached the end of string, check if substr is longest substr
               longestString = currentString
           }
       } else {
-          if (currentString.length > longestString.length) {
+          if (currentString.length > longestString.length) { // Once repeated char found in curr substr, if curr substring is longer, replace longestString
               longestString = currentString;
           }
-          let prevCharIdx = chars[s[i]]
-          currentString = s.slice(prevCharIdx+1,i) + s[i];
-          chars[s[i]] = i
+          let prevCharIdx = chars[s[i]] // Reset the index of char in the obj
+          currentString = s.slice(prevCharIdx+1,i) + s[i]; // Replace current string from after the occur of the repeated char to the current character
+          chars[s[i]] = i // reset char index in object
       }
   }
   return longestString.length
